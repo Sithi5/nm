3baca9af721859ec472a8f616681877d5a46f898
Sithi5 <ma.sithis@directbrico.com>
2023-04-19 20:06:46 +0200

    [chore]: use ft_printf instead of printf

--- src/display.c
+++ src/display.c
@@ -2,16 +2,16 @@
 
 void display_args(const t_args *args)
 {
-    printf("a_flag: %s\n", args->a_flag ? "true" : "false");
-    printf("g_flag: %s\n", args->g_flag ? "true" : "false");
-    printf("u_flag: %s\n", args->u_flag ? "true" : "false");
-    printf("r_flag: %s\n", args->r_flag ? "true" : "false");
-    printf("p_flag: %s\n", args->p_flag ? "true" : "false");
+    ft_printf("a_flag: %s\n", args->a_flag ? "true" : "false");
+    ft_printf("g_flag: %s\n", args->g_flag ? "true" : "false");
+    ft_printf("u_flag: %s\n", args->u_flag ? "true" : "false");
+    ft_printf("r_flag: %s\n", args->r_flag ? "true" : "false");
+    ft_printf("p_flag: %s\n", args->p_flag ? "true" : "false");
 
-    printf("Files:\n");
+    ft_printf("Files:\n");
     for (int i = 0; i < MAX_FILES && args->files_names[i] != NULL; i++)
     {
-        printf("  %s\n", args->files_names[i]);
+        ft_printf("  %s\n", args->files_names[i]);
     }
 }
 
